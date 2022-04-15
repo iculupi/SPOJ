f85de263f5c41330805e45c413006dc4995f2f31
Łukasz Pilarczyk <lupi@icutech.pl>
2022-04-15 12:57:11 +0200

    feat: Test1 done.

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+
+class Test1
+{
+    public static void main (String[] args)
+    {
+        int a = new Scanner(System.in).nextInt();
+        int b = new Scanner(System.in).nextInt();
+        System.out.print(a+b);
+    }
+}
